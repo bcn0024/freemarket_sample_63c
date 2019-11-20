e9b178e44eee5317e1e5c10ca961376ae9fdb12c
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2019-11-20 19:38:11 +0900

    バリデーション？

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :nickname, :email, :encrypted_password, :kanji_surname,:kanji_given_name,:kana_surname,:kana_given_name,:phone_number, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
