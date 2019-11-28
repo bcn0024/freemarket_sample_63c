3d0879fc108e5b44b73128b99a27d703896458ca
kazuki-sh <56525103+kazuki-sh@users.noreply.github.com>
2019-11-28 15:17:55 +0900

    firstcommit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # 
 
+  root to:  'users#new7'
   root to:  'products#index'
   
   resources :products, only: [:index, :create,:new]
