594e34339b7f6fa3d3958f09c60ed4a6456fa70d
bcn0024 <bcn0024@icloud.com>
2019-11-29 23:46:26 +0900

    aaa

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -19,6 +19,48 @@ class SignupController < ApplicationController
     @user = User.new
   end
 
+  def step3
+    # binding.pry
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    session[:kanji_surname] = user_params[:kanji_surname]
+    session[:kanji_given_name] = user_params[:kanji_given_name]
+    session[:kana_surname] = user_params[:kana_surname]
+    session[:kana_given_name] = user_params[:kana_given_name]
+    session[:birth_year] = user_params[:birth_year]
+    session[:birth_month] = user_params[:birth_month]
+    session[:birth_day] = user_params[:birth_day]
+    session[:phone_number] = user_params[:phone_number]
+    @user = User.new
+    @address = Address.new
+  end
+
+  def step4
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    session[:kanji_surname] = user_params[:kanji_surname]
+    session[:kanji_given_name] = user_params[:kanji_given_name]
+    session[:kana_surname] = user_params[:kana_surname]
+    session[:kana_given_name] = user_params[:kana_given_name]
+    session[:birth_year] = user_params[:birth_year]
+    session[:birth_month] = user_params[:birth_month]
+    session[:birth_day] = user_params[:birth_day]
+    session[:phone_number] = user_params[:phone_number]
+    session[:postal_code] = address_params[:postal_code]
+    session[:prefectures] = address_params[:prefectures]
+    session[:address] = address_params[:address]
+    session[:municipalities] = address_params[:municipalities]
+    session[:building] = address_params[:building]
+    session[:user_id] = user_params[:id]
+    @user = User.new
+    @address = Address.new
+  end
+
+
   def create
       @user=User.create(
       nickname: session[:nickname],
@@ -32,20 +74,25 @@ class SignupController < ApplicationController
       birth_year: session[:birth_year], 
       birth_month: session[:birth_month], 
       birth_day: session[:birth_day],
-      phone_number: user_params[:phone_number]
+      phone_number: session[:phone_number],
     )
-    if @user.save
-      redirect_to  new_signup_address_path(:signup_id)
-    else
-      render '/signup/step1'
-    end
+     @address=Address.create(
+      postal_code: session[:postal_code],
+      prefectures: session[:prefectures],
+      address: session[:address],
+      municipalities: session[:municipalities],
+      building: session[:building],
+      user_id: session[:user_id],
+     )
+    @user.save
+    @address.save
+    redirect_to  new_signup_address_path(:signup_id)
+    # else
+    #   render '/signup/step1'
+    # end
   end
 
-  def step3
-    @user = User.find(1)
-    @address = Address.new
-    # @address = @user.address
-  end
+  
 
 
 
@@ -67,6 +114,17 @@ class SignupController < ApplicationController
       :birth_month,
       :birth_day,
       :phone_number
+  )
+  end
+  def address_params
+    params.require(:address).permit(
+      :nickname, 
+      :postal_code,
+      :prefectures,
+      :address,
+      :municipalities,
+      :building,
+      :user_id
   )
   end
 end
