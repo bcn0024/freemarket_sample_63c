2cafc83a304badd576f77b8e71ad58e3eec81270
ryotokurata <t.d.o.a.its.m.l@i.softbank.jp>
2019-12-03 16:59:20 +0900

    修正2

--- config/routes.rb
+++ config/routes.rb
@@ -3,29 +3,19 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-<<<<<<< HEAD
   root to:  'products#index'
-=======
-  root to:  'users#exhibitedit'
->>>>>>> eeb69d93fac5a86c434446fefc45ca477cb67919
-  
+
   resources :products, only: [:index, :create,:new,:destroy] do
     resources :images, only: [:index,:destroy]
     member do
       get 'myproduct'
     end
   end
-<<<<<<< HEAD
-  resources :users, only: [:index, :create,:new,:destroy,:show] 
-=======
+
 
   resources :users, only: [:index, :create, :new, :show ,:destoroy] 
-  
 
- 
-  resources :mypage,only:[:index,:show,:destroy]
 
->>>>>>> eeb69d93fac5a86c434446fefc45ca477cb67919
   resources :signup do
     resources :addresses, only: [:index, :create,:new,]
     resources :cards, only: [:index, :create,:new,]
