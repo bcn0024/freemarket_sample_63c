f90d9da3980e22cf52ec18fb15147322f1d21cf6
kazuki-sh <56525103+kazuki-sh@users.noreply.github.com>
2019-12-04 12:49:12 +0900

    ファイルから画像を選べる機能を追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new]
 
   def index
     @user = User.new
