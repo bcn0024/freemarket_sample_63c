1b20956af16cc977a610f1e096e8b079bdc75518
kazuki-sh <56525103+kazuki-sh@users.noreply.github.com>
2019-12-05 17:25:44 +0900

    おもちゃ　導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -186,3 +186,33 @@ enjoyment_cd.children.create([{name: "邦楽"}, {name: "洋楽"}, {name: "アニ
 enjoyment_dvd.children.create([{name: "外国映画"}, {name: "日本映画"}, {name: "アニメ"}, {name: "TVドラマ"}, {name: "ミュージック"}, {name: "お笑い/バラエティ"}, {name: "スポーツ/フィットネス"}, {name: "キッズ/ファミリー"}, {name: "その他"}])
 enjoyment_record.children.create([{name: "邦楽"}, {name: "洋楽"}, {name: "その他"}])
 enjoyment_tvgame.children.create([{name: "家庭用ゲーム本体"}, {name: "家庭用ゲームソフト"}, {name: "携帯用ゲーム本体"}, {name: "携帯用ゲームソフト"}, {name: "PCゲーム"}, {name: "その他"},])
+
+
+# おもちゃ・ホビー・グッズ
+
+toys = Category.create(name: "おもちゃ・ホビー・グッズ")
+
+toys_toy = toys.children.create(name: "おもちゃ")
+toys_talentgoods = toys.children.create(name: "タレントグッズ")
+toys_comic = toys.children.create(name: "コミック/アニメグッズ")
+toys_card = toys.children.create(name: "トレーディングカード")
+toys_doll = toys.children.create(name: "フィギュア")
+toys_instrument = toys.children.create(name: "楽器/器材")
+toys_collection = toys.children.create(name: "コレクション")
+toys_miritary = toys.children.create(name: "ミリタリー")
+toys_art = toys.children.create(name: "美術品")
+toys_art_goods = toys.children.create(name: "アート用品")
+toys_etc = toys.children.create(name: "その他")
+
+
+toys_toy.children.create([{name: "キャラクターグッズ"},  {name: "ぬいぐるみ"},  {name: "小物/アクセサリー"},  {name: "模型/プラモデル"},  {name: "ミニカー"},  {name: "トイラジコン"},  {name: "プラモデル"},  {name: "ホビーラジコン"},  {name: "鉄道模型"},  {name: "その他"}])
+toys_talentgoods.children.create([{name: "アイドル"},  {name: "ミュージシャン"},  {name: "タレント/お笑い芸人"},  {name: "スポーツ選手"},  {name: "その他"}])
+toys_comic.children.create([{name: "ストラップ"}, {name: "キーホルダー"}, {name: "バッジ"}, {name: "カード"}, {name: "クリアファイル"}, {name: "ポスター"}, {name: "タオル"}, {name: "その他"}])
+toys_card.children.create([{name: "遊戯王"},  {name: "マジック：ザ・ギャザリング"},  {name: "ポケモンカードゲーム"},  {name: "デュエルマスターズ"},  {name: "バトルスピリッツ"},  {name: "プリパラ"},  {name: "アイカツ"},  {name: "カードファイト!! ヴァンガード"},  {name: "ヴァイスシュヴァルツ"},  {name: "プロ野球オーナーズリーグ"},  {name: "ベースボールヒーローズ"},  {name: "ドラゴンボール"},  {name: "スリーブ"},  {name: "その他"}])
+toys_doll.children.create([{name: "コミック/アニメ"},  {name: "特撮"},  {name: "ゲームキャラクター"},  {name: "SF/ファンタジー/ホラー"},  {name: "アメコミ"},  {name: "スポーツ"},  {name: "ミリタリー"},  {name: "その他"}])
+toys_instrument.children.create([{name: "エレキギター"},  {name: "アコースティックギター"},  {name: "ベース"},  {name: "エフェクター"},  {name: "アンプ"},  {name: "弦楽器"},  {name: "管楽器"},  {name: "鍵盤楽器"},  {name: "打楽器"},  {name: "和楽器"},  {name: "楽譜/スコア"},  {name: "レコーディング/PA機器"},  {name: "DJ機器"},  {name: "DTM/DAW"}, {name: "その他"}])
+toys_collection.children.create([{name: "武具"},  {name: "使用済切手/官製はがき"},  {name: "旧貨幣/金貨/銀貨/記念硬貨"},  {name: "印刷物"},  {name: "ノベルティグッズ"},  {name: "その他"}])
+toys_miritary.children.create([{name: "トイガン"},  {name: "個人装備"},  {name: "その他"}])
+toys_art.children.create([{name: "陶芸"},  {name: "ガラス"},  {name: "漆芸"},  {name: "金属工芸"},  {name: "絵画/タペストリ"},  {name: "版画"},  {name: "彫刻/オブジェクト"}, {name: "書"},  {name: "写真"},  {name: "その他"}])
+toys_art_goods.children.create([{name: "画材"},  {name: "額縁"},  {name: "その他"}])
+toys_etc.children.create([{name: "トランプ/UNO"},  {name: "カルタ/百人一首"},  {name: "ダーツ"},  {name: "ビリヤード"},  {name: "麻雀"},  {name: "パズル/ジグソーパズル"},  {name: "囲碁/将棋"},  {name: "オセロ/チェス"},  {name: "人生ゲーム"},  {name: "野球/サッカーゲーム"},  {name: "スポーツ"},  {name: "三輪車/乗り物"},  {name: "ヨーヨー"},  {name: "模型製作用品"},  {name: "鉄道"},  {name: "航空機"},  {name: "アマチュア無線"},  {name: "パチンコ/パチスロ"},  {name: "その他"}])
