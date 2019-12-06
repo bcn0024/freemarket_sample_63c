cb81932b2536bdcf539f9b2beffc22abf85e8b8c
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2019-12-06 19:21:08 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
 
 
   def show
-     @user = User.find(params[:id])
-     @products = @user.products
+    @user = User.find(params[:id])
+    @products = @user.products
   end
 
   def howtopay
