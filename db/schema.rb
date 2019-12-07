dc3258adb5420da7b588478ee89c938b77a1d0a6
kazuki-sh <56525103+kazuki-sh@users.noreply.github.com>
2019-12-07 13:47:45 +0900

    brand フォームを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_074417) do
+ActiveRecord::Schema.define(version: 2019_12_07_041008) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2019_12_06_074417) do
     t.string "address"
     t.string "building"
     t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
