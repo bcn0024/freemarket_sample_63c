f09b2aaed64e07cefa542400c316a9fa9f7a3c24
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2019-12-07 13:38:13 +0900

    コメントアウト修正途中

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,14 +7,4 @@ class Product < ApplicationRecord
   accepts_nested_attributes_for :images
   belongs_to :user, class_name: "User"
   belongs_to :buyer, class_name: "User"
-
-  # 単体テストの際、エラーが出るためコメントアウト
-  # 下記モデル作成後、その都度コメントアウト外す。
-
-  # has_many :categorys,dependent: :destroy
-  
-  # has_many :chats,dependent: :destroy
-  # has_many :favorites,dependent: :destroy
-  # belongs_to :brand
-  # belongs_to :category
 end
