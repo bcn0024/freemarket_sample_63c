bda52eea0cbe466697ad8cddbaaeb5a40a1a6266
bcn0024 <bcn0024@icloud.com>
2019-12-09 14:30:37 +0900

    カテゴリーのみ変更
    一部のみ変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,13 +10,12 @@ class Product < ApplicationRecord
   accepts_nested_attributes_for :brand
   belongs_to :user, class_name: "User"
   belongs_to :buyer, class_name: "User", optional:true
-
+  belongs_to :category
 
 
   # 単体テストの際、エラーが出るためコメントアウト
   # 下記モデル作成後、その都度コメントアウト外す。
 
-  # has_many :categorys,dependent: :destroy
   # has_many :chats,dependent: :destroy
   # has_many :favorites,dependent: :destroy
 
