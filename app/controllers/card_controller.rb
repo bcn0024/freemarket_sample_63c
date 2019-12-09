b3c3c5a94a64c25865e89e54855d96cf85e57ed2
ryotokurata <t.d.o.a.its.m.l@i.softbank.jp>
2019-12-09 11:04:16 +0900

    訂正

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -4,7 +4,7 @@ class CardController < ApplicationController
 
   def new
     card = Card.where(user_id: current_user.id)
-    redirect_to  user_card_path if card.exists?
+    redirect_to user_card_index_path(current_user.id) if card.exists?
   end
 
   def pay #payjpとCardのデータベース作成を実施します。
@@ -51,9 +51,9 @@ class CardController < ApplicationController
   end
 
   def index
-      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-      customer = Payjp::Customer.retrieve(@card.customer_id)
-      @default_card_information = customer.cards.retrieve(@card.card_id)
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    customer = Payjp::Customer.retrieve(@card.customer_id)
+    @default_card_information = customer.cards.retrieve(@card.card_id)
   end
 
   def set_card
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -97,7 +97,7 @@ class ProductsController < ApplicationController
  def pay #payjpとCardのデータベース作成を実施します。
   Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
   if params['payjp-token'].blank?
-    redirect_to  cardnew_products_path
+    redirect_to  cardnew_products_path(card.customer_id)
   else
     customer = Payjp::Customer.create(
     description: '登録テスト', #なくてもOK
@@ -107,9 +107,9 @@ class ProductsController < ApplicationController
     ) #念の為metadataにuser_idを入れましたがなくてもOK
     @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
     if @card.save
-      redirect_to cardshow_products_path
+      redirect_to cardshow_products_path(card.customer_id)
     else
-      redirect_to  cardnew_products_path
+      redirect_to  cardnew_products_path(card.customer_id)
     end
   end
 end
