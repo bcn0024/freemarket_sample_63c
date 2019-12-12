743a14f185eeac00206e5bc0904eb8b5ec62b546
kazuki-sh <56525103+kazuki-sh@users.noreply.github.com>
2019-12-12 23:32:19 +0900

    自動でスクロールするように

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,42 @@
 $(function(){
+  var intervalId;
+  setTimer();
 
+  function setTimer(){
+    intervalId = setInterval(autoClick, 5000);
+  }
 
+  function autoClick() {
+    $(".slide").children("a.next").click();
+  }
+
+  function changeImage($click){
+    var $current = $click.siblings(".container").children(".current");
+    var $new;
+    var findSelector = "";
+
+    if($click.hasClass("next")){
+      $new = $current.next();
+      findSelector = ":first-child";
+    } else {
+      $new = $current.prev();
+      findSelector = ":last-child"
+    }
+
+    if($new.length == 0) {
+      $new = $current.siblings(findSelector);
+    }
+    $current.removeClass("current");
+    $new.addClass("current");
+    setTimer();
+  }
+
+  $(".slide").on("click", "> a", function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    clearInterval(intervalId);
+    changeImage($(this));
+  });
 
   $("label").on("change", 'input[type="file"]', function(e) {
     var file =  e.target.files[0],
